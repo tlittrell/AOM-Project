3bc3ffe64aca390e3c010cc6f2960f4efe70db94
tlittrell <littrell.thomas@gmail.com>
2017-10-21 18:45:53 -0400

    Wrote file to read and merge data
    Merged all data, added year variable, and cleaned up names

---
+++ Clean and merge.R
@@ -0,0 +1,19 @@
+library(readxl)
+library(tidyverse)
+library(stringr)
+
+setwd("~/Dropbox (MIT)/AOM Project/Data/All Data")
+data.files = list.files(pattern="*.xlsx")
+
+data = data.frame()
+for (i in data.files) {
+  year = substr(i,start=1,stop = 4) %>% as.numeric()
+  temp = read_excel(i) %>%
+    mutate(Year = year,
+           Player = str_extract(Player, pattern = "^[A-Za-z\\s-\\.]+"))
+  data = rbind(data, temp)
+}
+
+
+
+
