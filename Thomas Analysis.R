b70bc8df7e880d2c8658008319d75f5f5b60443a
tlittrell <littrell.thomas@gmail.com>
2017-10-21 20:15:24 -0400

    Test commit

---
+++ Thomas Analysis.R
@@ -0,0 +1,2 @@
+setwd("~/Dropbox (Personal)/MIT/Fall 2017/Analytics of Operations Management/Project/AOM-Project")
+df = read.csv("Combined Data.csv")
--- test file.R
+++ test file.R
@@ -3,3 +3,5 @@
 #a second  change
 
 #third commit
+
+#fourth test
