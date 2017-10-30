b578efeb4f1b93e9635e94febdb67be498a12263
tlittrell <littrell.thomas@gmail.com>
2017-10-30 19:33:59 -0400

    Import data
    Selected columns and filtered on minutes played

--- Thomas Analysis.R
+++ Thomas Analysis.R
@@ -1,2 +1,7 @@
+library(tidyverse)
 setwd("~/Dropbox (Personal)/MIT/Fall 2017/Analytics of Operations Management/Project/AOM-Project")
+
 df = read.csv("Combined Data.csv")
+df2 = df %>%
+  filter(MP >= 500) %>%
+  select(-X,-Rk,-Pos,-Age,-Tm,-G,-GS, -X3P., -FG., -FG, -FGA, -FT., -TRB, -PTS) 
