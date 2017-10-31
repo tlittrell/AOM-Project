b0ad8cad86a2075eb05eff650ebe2d7e85014e30
tlittrell <littrell.thomas@gmail.com>
2017-10-30 20:01:59 -0400

    biScale and plots
    Scaled input data and added pipeline for plots

--- Thomas Analysis.R
+++ Thomas Analysis.R
@@ -1,7 +1,57 @@
 library(tidyverse)
+library(softImpute)
 setwd("~/Dropbox (Personal)/MIT/Fall 2017/Analytics of Operations Management/Project/AOM-Project")
 
+# Read in and clean data
 df = read.csv("Combined Data.csv")
 df2 = df %>%
-  filter(MP >= 500) %>%
-  select(-X,-Rk,-Pos,-Age,-Tm,-G,-GS, -X3P., -FG., -FG, -FGA, -FT., -TRB, -PTS) 
+  filter(MP >= 500) 
+
+
+# Run clusters and append to the data frame
+df_cluster = df2 %>% 
+  select(-Player, -MP, -Year, -X,-Rk,-Pos,-Age,-Tm,-G,-GS, -X3P., -FG., -FG, -FGA, -FT., -PTS) %>%
+  biScale()
+
+clusters = kmeans(df_cluster,5)
+clusters
+
+df2 = df2 %>%
+  mutate(cluster = factor(clusters$cluster))
+
+
+# Plots
+df2 %>%
+  ggplot() + 
+  aes(x = X3PA, y = X2PA, color = cluster) + 
+  geom_point()
+  
+df2 %>% 
+  ggplot() + 
+  aes(x = cluster, y = TRB) + 
+  geom_boxplot()
+
+df2 %>%
+  ggplot() +
+  aes(x = cluster, y = AST) +
+  geom_boxplot()
+
+df2 %>%
+  ggplot() +
+  aes(x = cluster, y = STL) +
+  geom_boxplot()
+
+df2 %>%
+  ggplot() +
+  aes(x = cluster, y = BLK) +
+  geom_boxplot()
+
+df2 %>%
+  ggplot() +
+  aes(x = cluster, y = TOV) +
+  geom_boxplot()
+
+df2 %>%
+  ggplot() +
+  aes(x = cluster, y = PF) +
+  geom_boxplot()
